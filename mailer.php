9624646e369e7195e7dbfed75d2919a36cd5f237
jenz <jenz@silpion.de>
2016-06-17 15:31:16 +0200

    changed the way options are used. added more Options

--- mailer.php
+++ mailer.php
@@ -1,19 +1,85 @@
 <?php
 
 include "PHPMailerAutoload.php";
-include "config.php";
+
 
 $mail = new PHPMailer();
+// Will be overridden by config.php
+$config = [
+    'mailerUsername' => "",
+    'mailerPassword' => "",
+    'files' => [],
+    'content' => '',
+    'recipients' => [],
+    'subject' => "",
+    'from' => [],
+];
+include "config.php";
 
-$file = ($argc>1)?$argv[1]:null;
-$content = ($argc>2)?$argv[2]:null;
+function startsWith($haystack, $needle)
+{
+    // search backwards starting from haystack length characters from the end
+    return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== false;
+}
+
+//Option Handling
+function startsWith2($hayStack, array $needles)
+{
+    foreach ($needles as $needle) {
+
+        if (startsWith($hayStack, $needle)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+foreach ($argv as $arg) {
+    if (startsWith2($arg, ["--attachment", "-a"])) {
+        $_tmp = explode("=", $arg, 2);
+        $config['files'][] = $_tmp[1];
+    }
+    if (startsWith2($arg, ["--body", "-b"])) {
+        $_tmp = explode("=", $arg, 2);
+        $config['content'] = $_tmp[1];
+    }
+    if (startsWith2($arg, ["--subject", "-s"])) {
+        $_tmp = explode("=", $arg, 2);
+        $config['subject'] = $_tmp[1];
+    }
+    if (startsWith2($arg, ["--recipient", "-r"])) {
+        $_tmp = explode("=", $arg, 2);
+        $rec = $_tmp[1];
+
+        $rec = explode("=>", $rec, 2);
+        if (count($rec) == 2) {
+            $config['recipients'][$rec[0]] = $rec[1];
+        } else {
+            $config['recipients'][$rec[0]] = "";
+        }
+    }
+    if (startsWith2($arg, ["--from", "-f"])) {
+        $_tmp = explode("=", $arg, 2);
+        $rec = $_tmp[1];
+
+        $rec = explode("=>", $rec, 2);
+        if (count($rec) == 2) {
+            $config['from'][$rec[0]] = $rec[1];
+        } else {
+            $config['from'][$rec[0]] = "";
+        }
+    }
+}
+
 //Tell PHPMailer to use SMTP
 $mail->isSMTP();
 //Enable SMTP debugging
 // 0 = off (for production use)
 // 1 = client messages
 // 2 = client and server messages
-$mail->SMTPDebug = 2;
+$mail->SMTPDebug = 0;
 //Ask for HTML-friendly debug output
 $mail->Debugoutput = 'echo';
 //Set the hostname of the mail server
@@ -28,42 +94,49 @@ $mail->SMTPSecure = 'tls';
 //Whether to use SMTP authentication
 $mail->SMTPAuth = true;
 //Username to use for SMTP authentication - use full email address for gmail
-$mail->Username = $mailerUsername;
+$mail->Username = $config['mailerUsername'];
 //Password to use for SMTP authentication
-$mail->Password = $mailerPassword;
+$mail->Password = $config['mailerPassword'];
 //Set who the message is to be sent from
-$mail->setFrom('loki@dieser-loki.de', 'Marcus "Loki" Jenz');
+foreach ($config['from'] as $address=>  $name) {
+
+    $mail->setFrom($address, $name);
+}
+//$mail->setFrom('loki@dieser-loki.de', 'Marcus "Loki" Jenz');
 //Set an alternative reply-to address
 //$mail->addReplyTo('replyto@example.com', 'First Last');
 //Set who the message is to be sent to
-$mail->addAddress('loki@silpion.de', 'Loki');
-//Set the subject line
-$mail->Subject = 'PHPMailer GMail SMTP test';
-$mail->Body = "Das ist ein Test";
-$mail->AltBody = 'Das ist ein Test';
 
-if($content){
+foreach ($config['recipients'] as $address=>  $name) {
+
+    $mail->addAddress($address, $name);
+}
+
+
+//$mail->addAddress('loki@silpion.de', 'Loki');
+//Set the subject line
+$mail->Subject = $config['subject'];
+$mail->Body = "This is a Test-Email";
+//$mail->AltBody = 'Das ist ein Test';
+
+if ($config['content']) {
     //Read an HTML message body from an external file, convert referenced images to embedded,
     //convert HTML into a basic plain-text alternative body
-    $pathInfo = pathinfo($content);
-    $contenfFile= $pathInfo['basename'];
-    $contenfDir= $pathInfo['dirname'];
-    echo "\n$content\n";
+    $pathInfo = pathinfo($config['content']);
 
-    $mail->msgHTML(file_get_contents($content), dirname(__FILE__));
-//    $mail->msgHTML("<h1>hello World<br/></h1>Das Laeuft!<br/>", dirname(__FILE__));
+    $mail->msgHTML(file_get_contents($config['content']), dirname(__FILE__));
 }
 //exit(0);
 //Replace the plain text body with one created manually
 //Attach an image file
-if($file){
+foreach($config['files'] as $file) {
     $mail->addAttachment($file);
 }
 
 //send the message, check for errors
 if (!$mail->send()) {
-    echo "\n\n\nMailer Error: " . $mail->ErrorInfo;
+    echo "\n\n\nMailer Error: ".$mail->ErrorInfo."\n";
 } else {
-    echo "Message sent!";
+    echo "Message sent!\n";
 }
 exit(0);
